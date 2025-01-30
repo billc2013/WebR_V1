ab7624de12c4e0049a7fa49e77e1ffa453a6439c
billc2013 <williamchurch17@gmail.com>
2025-01-30 15:06:57 -0500

    changed reference to supabase URL for import

--- public/src/supabase/supabaseClient.js
+++ public/src/supabase/supabaseClient.js
@@ -1,5 +1,5 @@
 // supabaseClient.js
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2';
 
 const SUPABASE_URL = 'https://jdhsebrydbhdbvdtskok.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpkaHNlYnJ5ZGJoZGJ2ZHRza29rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgxNzg3MzMsImV4cCI6MjA1Mzc1NDczM30.R5CD1BqxanjIMgsS9AK5fqM4n5CC51ZLSAeEFdPipgk';
